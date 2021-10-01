838eab5fdd709b4d46ad826df37dd714d720f57f
Joshua Andes <andesjoshua@comcast.net>
2021-10-01 13:29:16 -0400

    commit tree 67671d20a6192b14a2e6ef6dfbf19a04ede02e44

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+async function submitData(inputName, inputEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: inputName,
+            email: inputEmail
+        })    
+})
+        .then(resp => resp.json())
+        .then(json => {
+            const newItemId = json.id;
+            const p = document.createElement('p')
+            p.textContent = newItemId;
+            const body = document.querySelector('body');
+            body.append(p);
+        })
+        .catch(someError => {
+            const p = document.createElement('p')
+            p.textContent = someError.message;
+            const body = document.querySelector('body');
+            body.append(p);
+        })
+       
+
+
+
+    }
